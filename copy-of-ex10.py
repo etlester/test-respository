afdb32957e0ab5f03b52b7770634902de2d0bdb3
etlester <Etlester1@gmail.com>
2016-02-25 19:38:41 -0700

    added some stuff
    added two files

---
+++ copy-of-ex10.py
@@ -0,0 +1,24 @@
+# Tutorial
+
+#This is my tutorial repository.
+
+tabby_cat = ".\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+
+fat_cat = """
+I'l do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grasspwd
+
+"""
+
+
+print "how many times will github crash?"
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
